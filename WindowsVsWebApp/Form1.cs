3564593dd9563794a935ce4cb4fd6ed2cc7b4918
SEC-MASUM <masum.jess42@gmail.com>
2021-02-25 16:36:18 +0600

    Create Empty Windows Form App Project and .aspx file

---
+++ WindowsVsWebApp/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsVsWebApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string name;
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            name = nameTextBox.Text;
+            nameTextBox.Clear();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            outputTextBox.Text = name;
+        }
+    }
+}
