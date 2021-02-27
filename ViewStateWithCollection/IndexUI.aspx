6f0b37986fbe33f76d5176981eacf39ba958cc73
SEC-MASUM <masum.jess42@gmail.com>
2021-02-27 11:27:05 +0600

    Complete Project

---
+++ ViewStateWithCollection/IndexUI.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="IndexUI.aspx.cs" Inherits="ViewStateWithCollection.IndexUI" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Name"></asp:Label>
+            <asp:TextBox ID="nameTextBox" runat="server" Width="158px"></asp:TextBox>
+            <br />
+            <asp:Button ID="saveButton" runat="server" OnClick="saveButton_Click" Text="Save" />
+            <asp:Button ID="showAllButton" runat="server" Text="Show All" OnClick="showAllButton_Click" />
+        </div>
+        <asp:ListBox ID="nameListBox" runat="server" Height="124px" Width="201px"></asp:ListBox>
+    </form>
+</body>
+</html>
---
+++ ViewStateWithCollection/IndexUI.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ViewStateWithCollection
+{
+    public partial class IndexUI : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+        List<string> namesList = new List<string>();
+        protected void saveButton_Click(object sender, EventArgs e)
+        {
+            string name = nameTextBox.Text;
+            if (ViewState["nameState"]!=null)
+            {
+                namesList = (List<string>)ViewState["nameState"];
+            }
+            namesList.Add(name);
+            ViewState["nameState"] = namesList;
+        }
+
+        protected void showAllButton_Click(object sender, EventArgs e)
+        {
+            namesList = (List<string>)ViewState["nameState"];
+            nameListBox.Items.Clear();
+            foreach (var data in namesList)
+            {
+                nameListBox.Items.Add(data);
+            }
+        }
+    }
+}
