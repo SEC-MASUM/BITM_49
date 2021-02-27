a7e5105b355b576842c8f30a867d9f667d025ebf
SEC-MASUM <masum.jess42@gmail.com>
2021-02-27 18:55:12 +0600

    Learn about Value Type Vs Reference Type

---
+++ ValueTypeVsReferenceTypeApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ValueTypeVsReferenceTypeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person aPerson = new Person();
+            aPerson.firstName = "Md";
+            aPerson.middleName = "Abid";
+            aPerson.lastName = "Hasan";
+            Change(aPerson);
+            Console.WriteLine(aPerson.lastName);
+
+        }
+
+        public static void Change(Person person)
+        {
+            person.firstName = "Suad";
+            person.lastName = "Jim";
+        }
+    }
+}
