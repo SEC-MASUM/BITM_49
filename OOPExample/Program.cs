873f75f2e72947e736131fc7d091998783864883
SEC-MASUM <masum.jess42@gmail.com>
2021-03-06 23:12:54 +0600

    Change OOP Code

--- OOPExample/Program.cs
+++ OOPExample/Program.cs
@@ -10,6 +10,15 @@ namespace OOPExample
     {
         static void Main(string[] args)
         {
+            Person person1;
+            person1 = new Person();
+
+            person1.firstName = "Md.";
+            person1.middleName = "Masum";
+            person1.lastName = "Billah";
+
+            string fullname = person1.GetFullName();
+            Console.WriteLine(fullname);
         }
     }
 }
